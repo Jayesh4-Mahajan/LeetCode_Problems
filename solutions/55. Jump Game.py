9b7b5851e7aab16d68274a1fafd2b49b12e81472
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2021-12-29 20:09:35 +0530

    753 ms

--- solutions/55. Jump Game.py
+++ solutions/55. Jump Game.py
@@ -1,14 +1,21 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-        if nums[0] == 0:
-            return False
-        for i in range(nums[0]):
-            if self.canJump(nums[i+1:]):
-                return True
-        return False
+#         Time Limit Exceeded
+        # if len(nums) == 1:
+        #     return True
+        # if nums[0] == 0:
+        #     return False
+        # for i in range(nums[0]):
+        #     if self.canJump(nums[i+1:]):
+        #         return True
+        # return False
         
+#         Using Greedy : Going in reverse
+        end = len(nums) - 1
+        for i in range(len(nums)-1, -1, -1):
+            if i + nums[i] >= end:
+                end = i
+        return True if end == 0 else False
         
         
         
