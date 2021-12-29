18e3502dc450ed4a0dcfbda67b7674e07679e5e1
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2021-12-29 19:18:04 +0530

    N/A

---
+++ solutions/53. Maximum Subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+#         Time Limit Exceeded
+        # if len(nums)==1: return nums[0]        
+        # maxSum = nums[0]
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)+1):
+        #         a = sum(nums[i:j])
+        #         if maxSum < a:
+        #             maxSum = a
+        # return maxSum
+            
+        
