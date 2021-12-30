d61139c694fdf3bdf045cdfd37d0a0404605e0e5
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2021-12-30 19:57:32 +0530

    29 ms

--- solutions/62. Unique Paths.py
+++ solutions/62. Unique Paths.py
@@ -4,8 +4,10 @@ class Solution:
         visited = {(1,1): 1, (2,1): 1, (1,2): 1}
 ​
         def paths(m, n):
-            if (m,n) in visited: 
+            if (m,n) in visited:
                 return visited[(m,n)]
+            elif (n, m) in visited:
+                return visited[(n,m)]
             elif m -1 > 0 and n -1 > 0:
                 visited[(m-1,n)] = paths(m-1,n)
                 visited[(m, n-1)] = paths(m,n-1)
@@ -17,5 +19,4 @@ class Solution:
                 visited[(m, n-1)] = paths(m,n-1)
                 return visited[(m, n-1)]         
         
-        visited[(m, n)] = paths(m, n)
-        return visited[(m, n)]
+        return paths(m, n)
