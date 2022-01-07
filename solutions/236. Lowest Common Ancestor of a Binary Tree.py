7a82cdea70bd0e4084ed56346a9d695348147dca
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2022-01-07 19:26:56 +0530

    72 ms

--- solutions/236. Lowest Common Ancestor of a Binary Tree.py
+++ solutions/236. Lowest Common Ancestor of a Binary Tree.py
@@ -5,41 +5,54 @@
 #         self.left = None
 #         self.right = None
 ​
-def inOrder(root,res):
-    if root.left:
-        inOrder(root.left, res)
-    if root:
-        res.append(root)
-    if root.right:
-        inOrder(root.right, res)
-    return res
+# def inOrder(root,res):
+#     if root.left:
+#         inOrder(root.left, res)
+#     if root:
+#         res.append(root)
+#     if root.right:
+#         inOrder(root.right, res)
+#     return res
 ​
-def preOrder(root, res):
-    if root:
-        res.append(root.val)
-    if root.left:
-        preOrder(root.left, res)
-    if root.right:
-        preOrder(root.right, res)
-    return res
+# def preOrder(root, res):
+#     if root:
+#         res.append(root.val)
+#     if root.left:
+#         preOrder(root.left, res)
+#     if root.right:
+#         preOrder(root.right, res)
+#     return res
 ​
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        inn = []
-        pre = []
-        inn = inOrder(root, inn)
-        pre = preOrder(root, pre)
-        mp = {}
-        for k, v in enumerate(inn):
-            mp[v.val] = k
-        pIdx = mp[p.val]
-        qIdx = mp[q.val]
-        for r in pre:
-            rootIdx = mp[r]
-            if (pIdx <= rootIdx <= qIdx) or (qIdx <= rootIdx <= pIdx):
-                return inn[rootIdx]
+#         First solution
+        # inn = []
+        # pre = []
+        # inn = inOrder(root, inn)
+        # pre = preOrder(root, pre)
+        # mp = {}
+        # for k, v in enumerate(inn):
+        #     mp[v.val] = k
+        # pIdx = mp[p.val]
+        # qIdx = mp[q.val]
+        # for r in pre:
+        #     rootIdx = mp[r]
+        #     if (pIdx <= rootIdx <= qIdx) or (qIdx <= rootIdx <= pIdx):
+        #         return inn[rootIdx]
         
         
+#         Second solution: recursively
+​
+#         Base Case
+        if root is None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
         
+        if left is not None and right is not None:
+            return root
+        if left is None:
+            return right
+        return left
             
         
