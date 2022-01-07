84fe2728ce17534b37269a039ce7e0bcb82348af
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2022-01-07 09:49:04 +0530

    67 ms

--- solutions/144. Binary Tree Preorder Traversal.py
+++ solutions/144. Binary Tree Preorder Traversal.py
@@ -5,19 +5,29 @@
 #         self.left = left
 #         self.right = right
 ​
-def printNode(root,res):
-    if root:
-        res.append(root.val)
-    else:
-        return res
-    if root.left:
-        printNode(root.left,res)
-    if root.right:
-        printNode(root.right,res)
-    return res
+# def printNode(root,res):
+#     if root:
+#         res.append(root.val)
+#     else:
+#         return res
+#     if root.left:
+#         printNode(root.left,res)
+#     if root.right:
+#         printNode(root.right,res)
+#     return res
         
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []            
-        return printNode(root,res)
+        # res = []            
+        # return printNode(root,res)
+        
+        res = []
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return res
         
