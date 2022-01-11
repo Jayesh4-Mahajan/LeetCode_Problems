fe15cc03ed6c12c6d3e1f04cc0222f9d26513bd4
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2022-01-11 17:26:50 +0530

    139 ms

--- solutions/653. Two Sum IV - Input is a BST.py
+++ solutions/653. Two Sum IV - Input is a BST.py
@@ -5,32 +5,45 @@
 #         self.left = left
 #         self.right = right
 ​
-def inOrder(root, res):
-    if root.left:
-        inOrder(root.left, res)
-    if root:
-        res.append(root.val)
-    if root.right:
-        inOrder(root.right, res)
-    return res
+#  Solution 1:
 ​
+# def inOrder(root, res):
+#     if root.left:
+#         inOrder(root.left, res)
+#     if root:
+#         res.append(root.val)
+#     if root.right:
+#         inOrder(root.right, res)
+#     return res
+​
+# class Solution:
+#     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+#         if not root: return False
+#         res = []
+#         res = inOrder(root, res)
+#         print(res)
+#         c1 = 0
+#         c2 = len(res) - 1
+#         while c1 < c2:
+#             v = res[c1]+res[c2]
+#             if v == k:
+#                 return True
+#             elif v < k:
+#                 c1 += 1
+#             else:
+#                 c2 -= 1
+#         return False
+            
+#     Solution 2: Using HashMap
+    
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        if not root: return False
-        res = []
-        res = inOrder(root, res)
-        print(res)
-        c1 = 0
-        c2 = len(res) - 1
-        while c1 < c2:
-            v = res[c1]+res[c2]
-            if v == k:
+        def dfs(root, seen):
+            if not root: return False
+            complement = k - root.val
+            if complement in seen:
                 return True
-            elif v < k:
-                c1 += 1
-            else:
-                c2 -= 1
-        return False
-            
-            
+            seen.add(root.val)
+            return dfs(root.left, seen) or dfs(root.right, seen)
         
+        return dfs(root, set())
