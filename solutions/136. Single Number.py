21361ff994c77166e45fb6cf8426bf990ecd6a2e
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2022-01-13 12:51:26 +0530

    212 ms

--- solutions/136. Single Number.py
+++ solutions/136. Single Number.py
@@ -1,12 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        nums.sort()
-        i = 0
-        while i < len(nums) - 1:
-            if nums[i] != nums[i+1]:
-                return nums[i]
-            i += 2
-        return nums[-1]
-            
+#         Solution 1
+        # if len(nums) == 1:
+        #     return nums[0]
+        # nums.sort()
+        # i = 0
+        # while i < len(nums) - 1:
+        #     if nums[i] != nums[i+1]:
+        #         return nums[i]
+        #     i += 2
+        # return nums[-1]
+        
+#         Solution 2
+        if len(nums) == 1: return nums[0]
+        return 2*sum(set(nums)) - sum(nums)
+        
